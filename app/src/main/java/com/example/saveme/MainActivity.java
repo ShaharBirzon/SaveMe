9736b2966876fa047a59c70b661263a0e4dbd43e
AyalMeitar <ayal.meitar@mail.huji.ac.il>
2020-07-26 11:57:47 +0300

    added on click (need to add - go into the category when clicked), and on long click - for deleting a category.

--- app/src/main/java/com/example/saveme/MainActivity.java
+++ app/src/main/java/com/example/saveme/MainActivity.java
@@ -6,7 +6,6 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -14,7 +13,6 @@ import com.example.saveme.utils.FirebaseMediate;
 import com.example.saveme.utils.MyPreferences;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // checks if needs to sign up or log in
         if (MyPreferences.isFirstTime(getApplicationContext())) {
             categoryList = FirebaseMediate.getDefaultCategories();
             User user = new User(categoryList);
@@ -34,17 +34,28 @@ public class MainActivity extends AppCompatActivity {
             categoryList = FirebaseMediate.getUserCategories();
         }
 
+        // initializes the recycler view and the adapter
+        initializeRecyclerView();
+
+        // when a category is clicked
+        onCategoryClicked();
+
+        // when a category is long clicked
+        onCategoryLongClicked();
+    }
+
+    private void initializeRecyclerView() {
+        //set recycler view and category adapter
         recyclerView = findViewById(R.id.category_recycler);
         recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
-
-//      recyclerView.setAdapter(new CategoryAdapter(categoryList)); todo changed to the lines below ok?
-        //set category adapter
         categoryAdapter = new CategoryAdapter(categoryList);
         recyclerView.setAdapter(categoryAdapter);
+    }
 
-        // builder for the delete dialog of long click
-        final AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
-
+    /*
+    when a category is clicked
+    */
+    private void onCategoryClicked() {
         // click listener - to go inside a category
         categoryAdapter.setCategoryClickListener(new CategoryClickListener() {
             @Override
@@ -54,6 +65,14 @@ public class MainActivity extends AppCompatActivity {
                 //todo move to the category activity
             }
         });
+    }
+
+    /*
+    when a category is long clicked
+     */
+    private void onCategoryLongClicked() {
+        // builder for the delete dialog of long click
+        final AlertDialog.Builder deleteAlertBuilder = new AlertDialog.Builder(this);
 
         // long click listener - if wants to delete a category
         categoryAdapter.setCategoryLongClickListener(new CategoryLongClickListener() {
@@ -61,12 +80,12 @@ public class MainActivity extends AppCompatActivity {
             public void onCategoryLongClicked(int position) {
                 Log.d("category long clicked", "category was long clicked");
 
-                builder1.setMessage("Are you sure you want do delete?");
-                builder1.setCancelable(true);
+                deleteAlertBuilder.setMessage("Are you sure you want do delete?");
+                deleteAlertBuilder.setCancelable(true);
                 final Category category_to_delete = categoryList.get(position); //todo check how to get current category like this or from adapter?
 
                 //if wants to delete for sure
-                builder1.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                deleteAlertBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         categoryAdapter.deleteCategory(category_to_delete);
@@ -75,17 +94,14 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
-                builder1.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                deleteAlertBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         dialog.cancel();
                     }
                 });
-
-                AlertDialog alert11 = builder1.create();
-                alert11.show();
+                AlertDialog DeleteAlertDialog = deleteAlertBuilder.create();
+                DeleteAlertDialog.show();
             }
         });
-
-
     }
 }
