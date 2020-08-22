e6a1c1c65a94c09e72846901b0defcfa6d8020ce
AyalMeitar <ayal.meitar@mail.huji.ac.il>
2020-08-22 19:06:50 +0300

    Option to add and edit documents with on click

--- app/src/main/java/com/example/saveme/document/DocumentActivity.java
+++ app/src/main/java/com/example/saveme/document/DocumentActivity.java
@@ -13,24 +13,57 @@ import com.example.saveme.category.CategoryActivity;
 public class DocumentActivity extends AppCompatActivity {
 
     private EditText documentTitleET;
+    private EditText documentCommentET;
+    private EditText documentExpirationDateET;
+    private String callReason;
+    private int position;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_document);
         documentTitleET = findViewById(R.id.et_document_title);
+        documentCommentET = findViewById(R.id.et_comment);
+        documentExpirationDateET = findViewById(R.id.et_expiration_date);
+        // todo add other
+
+        Intent intentCreatedMe = getIntent();
+        callReason = intentCreatedMe.getStringExtra("call_reason");
+        if (callReason.equals("edit_document")) {
+            String title = intentCreatedMe.getStringExtra("document_title");
+            documentTitleET.setText(title);
+            String comment = intentCreatedMe.getStringExtra("document_comment");
+            documentCommentET.setText(comment);
+            String expirationDate = intentCreatedMe.getStringExtra("document_expiration_date");
+            documentExpirationDateET.setText(expirationDate);
+            // todo for others
+            position = intentCreatedMe.getIntExtra("position", -1);
+
+        }
     }
 
     public void onClickSaveDocumentButton(View view) {
-        Intent intent = new Intent( DocumentActivity.this, CategoryActivity.class);
-        addDocumentToFirebase();
-        intent.putExtra("call_from", "new_document");
-        intent.putExtra("document_title", documentTitleET.getText().toString());
-        startActivity(intent);
+        Intent intentBack = new Intent(DocumentActivity.this, CategoryActivity.class);
+
+        // todo check if call_from is needed
+        if (callReason.equals("edit_document")) {
+            intentBack.putExtra("call_from", "edit_document");
+            intentBack.putExtra("document_position", position);
+        }
+        if (callReason.equals("new_document")) {
+            addDocumentToFirebase();
+            intentBack.putExtra("call_from", "new_document");
+        }
+
+        intentBack.putExtra("document_title", documentTitleET.getText().toString());
+        intentBack.putExtra("document_comment", documentCommentET.getText().toString());
+        intentBack.putExtra("document_expiration_date", documentExpirationDateET.getText().toString());
+        //todo add others
+        setResult(RESULT_OK, intentBack);
         finish();
     }
 
     private void addDocumentToFirebase() {
-
+        // todo maybe move to CategoryActivity
     }
 }
